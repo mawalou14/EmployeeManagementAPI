1a5f23ae1a0583581d5da918a04f50c64746544e
mawalou14 <awaloum213@gmail.com>
2025-02-08 01:56:06 +0100

    Create Auth Entities

---
+++ EmployeeManagementSystem.Domain/DTOs/AuthResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace EmployeeManagementSystem.Domain.DTOs
+{
+    public class AuthResponseDto
+    {
+        public string Token { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+    }
+}
---
+++ EmployeeManagementSystem.Domain/DTOs/LoginUserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace EmployeeManagementSystem.Domain.DTOs
+{
+	public class LoginUserDto
+	{
+		public string Email { get; set; } = string.Empty;
+		public string Password { get; set; } = string.Empty;
+	}
+}
