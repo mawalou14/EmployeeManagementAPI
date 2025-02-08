9c4ed6488be4d1dfe14b1a45c2451630ff8f706f
mawalou14 <awaloum213@gmail.com>
2025-02-08 02:28:20 +0100

    Add Validators, Contracts, Extensions

---
+++ EmployeeManagementSystem.Application/Contracts/IUserAccountRepository.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeManagementSystem.Domain.DTOs;
+using EmployeeManagementSystem.Domain.Responses;
+using EmployeeManagementSystem.Domain.Services.Contracts;
+
+namespace EmployeeManagementSystem.Application.Contracts
+{
+    public interface IUserAccountRepository
+    {
+        Task<GeneralResponse> CreateAsync(Register user);
+        Task<LoginResponse> SignInAsync(Login user);
+        Task<LoginResponse> RefreshTokenAsync(RefreshToken token);
+
+    }
+}
